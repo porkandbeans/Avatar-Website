d2babac811163135182a32e5107fa6527bb83b18
GoKritz <voter96@gmail.com>
2022-07-07 16:53:10 +1200

    Ready to show people, but more work can be done.

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# simple bash script I wrote to make pushing a little bit easier
+
+CMESG=$1
+if [ -z "$CMESG" ]
+then
+	CMESG="lazy auto push"
+fi
+
+sudo cat /root/github_keys
+git add -A
+git commit -m "$CMESG"
+git push
+clear
+echo "Done! screen cleared to protect github key."
